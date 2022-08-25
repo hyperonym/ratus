88736f24d6404b0a6e8292df1b2a66f7549bc27d
Yichao 'Peak' Ji <peakji@me.com>
2022-08-25 23:14:43 +0800

    feat(controller): add general swagger annotations

---
+++ internal/controller/controller.go
@@ -0,0 +1,20 @@
+// Package controller implements controllers for API endpoints.
+package controller
+
+// @title        Ratus
+// @version      v1
+// @description  Ratus API Specification
+
+// @contact.name  GitHub
+// @contact.url   https://github.com/hyperonym/ratus
+
+// @license.name  Mozilla Public License Version 2.0
+// @license.url   https://www.mozilla.org/en-US/MPL/2.0/
+
+// @basePath  /v1
+
+// @tag.name  topics
+// @tag.name  tasks
+// @tag.name  promises
+// @tag.name  health
+// @tag.name  metrics
