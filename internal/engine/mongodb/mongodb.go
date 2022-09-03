703174c09f3e6ce4f8f2215759dc1abef7ecf6ed
Yichao 'Peak' Ji <peakji@me.com>
2022-09-04 01:18:19 +0800

    feat(mongodb): add atomic fallback flags

--- internal/engine/mongodb/mongodb.go
+++ internal/engine/mongodb/mongodb.go
@@ -2,9 +2,11 @@
 package mongodb
 
 import (
+	"sync/atomic"
 	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 // Config contains configurations for the MongoDB storage engine.
@@ -26,4 +28,65 @@ type Engine struct {
 	client     *mongo.Client
 	database   *mongo.Database
 	collection *mongo.Collection
+
+	// Atomic fallback flags: -1 = disabled, 0 = auto, 1 = enabled.
+	fallbackPoll          *atomic.Int32
+	fallbackCommit        *atomic.Int32
+	fallbackUpsertTasks   *atomic.Int32
+	fallbackUpsertTask    *atomic.Int32
+	fallbackInsertPromise *atomic.Int32
+	fallbackUpsertPromise *atomic.Int32
+}
+
+// New creates a new MongoDB storage engine instance.
+func New(c *Config) (*Engine, error) {
+	g := Engine{
+		config:                c,
+		fallbackPoll:          &atomic.Int32{},
+		fallbackCommit:        &atomic.Int32{},
+		fallbackUpsertTasks:   &atomic.Int32{},
+		fallbackUpsertTask:    &atomic.Int32{},
+		fallbackInsertPromise: &atomic.Int32{},
+		fallbackUpsertPromise: &atomic.Int32{},
+	}
+
+	// Create a new client without actually connecting to the deployment.
+	// Initialization processes that requires I/O should happen in Open.
+	var err error
+	g.client, err = mongo.NewClient(options.Client().ApplyURI(c.URI))
+	if err != nil {
+		return nil, err
+	}
+
+	// Get handles for the database and the collection.
+	g.database = g.client.Database(c.Database)
+	g.collection = g.database.Collection(c.Collection)
+
+	// Disable transparent fallbacks if required.
+	if c.DisableAutoFallback {
+		g.Fallback(-1)
+	}
+
+	// Disable atomic polling if required.
+	if c.DisableAtomicPoll {
+		g.fallbackPoll.Store(1)
+	}
+
+	return &g, nil
+}
+
+// Collection returns the handle for the task collection.
+func (g *Engine) Collection() *mongo.Collection {
+	return g.collection
+}
+
+// Fallback sets all fallback flags to the given value.
+func (g *Engine) Fallback(v int32) *Engine {
+	g.fallbackPoll.Store(v)
+	g.fallbackCommit.Store(v)
+	g.fallbackUpsertTasks.Store(v)
+	g.fallbackUpsertTask.Store(v)
+	g.fallbackInsertPromise.Store(v)
+	g.fallbackUpsertPromise.Store(v)
+	return g
 }
