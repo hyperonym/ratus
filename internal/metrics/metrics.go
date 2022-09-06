5e5f84308e3930f88fe39fda4c6fb1bfbc1ef8ed
Yichao 'Peak' Ji <peakji@me.com>
2022-09-07 02:47:03 +0800

    refactor(metrics): add constant for labels

--- internal/metrics/metrics.go
+++ internal/metrics/metrics.go
@@ -6,13 +6,22 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promauto"
 )
 
+// Name constants for metrics labels.
+const (
+	labelTopic      = "topic"
+	labelProducer   = "producer"
+	labelConsumer   = "consumer"
+	labelEndpoint   = "endpoint"
+	labelStatusCode = "status_code"
+)
+
 var (
 	// Request response time in seconds.
 	RequestHistogram = promauto.NewHistogramVec(prometheus.HistogramOpts{
 		Name:    "ratus_request_duration_seconds",
 		Help:    "Request response time in seconds",
 		Buckets: []float64{0.01, 0.1, 0.5, 1, 2, 5},
-	}, []string{"topic", "endpoint", "status_code"})
+	}, []string{labelTopic, labelEndpoint, labelStatusCode})
 
 	// Periodic background jobs execution time in seconds.
 	ChoreHistogram = promauto.NewHistogram(prometheus.HistogramOpts{
@@ -25,29 +34,29 @@ var (
 	DelayGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
 		Name: "ratus_task_schedule_delay_seconds",
 		Help: "Task schedule delay in seconds",
-	}, []string{"topic", "producer", "consumer"})
+	}, []string{labelTopic, labelProducer, labelConsumer})
 
 	// Task execution time in seconds.
 	ExecutionGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
 		Name: "ratus_task_execution_duration_seconds",
 		Help: "Task execution time in seconds",
-	}, []string{"topic", "producer", "consumer"})
+	}, []string{labelTopic, labelProducer, labelConsumer})
 
 	// Total number of tasks produced.
 	ProducedCounter = promauto.NewCounterVec(prometheus.CounterOpts{
 		Name: "ratus_task_produced_count_total",
 		Help: "Total number of tasks produced",
-	}, []string{"topic", "producer"})
+	}, []string{labelTopic, labelProducer})
 
 	// Total number of tasks consumed.
 	ConsumedCounter = promauto.NewCounterVec(prometheus.CounterOpts{
 		Name: "ratus_task_consumed_count_total",
 		Help: "Total number of tasks consumed",
-	}, []string{"topic", "producer", "consumer"})
+	}, []string{labelTopic, labelProducer, labelConsumer})
 
 	// Total number of tasks committed.
 	CommittedCounter = promauto.NewCounterVec(prometheus.CounterOpts{
 		Name: "ratus_task_committed_count_total",
 		Help: "Total number of tasks committed",
-	}, []string{"topic", "producer", "consumer"})
+	}, []string{labelTopic, labelProducer, labelConsumer})
 )
