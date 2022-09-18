466de76a5811ea486cccdc2eefb0446bf08fbea0
Yichao 'Peak' Ji <peakji@me.com>
2022-09-19 01:21:38 +0800

    feat(memdb): add snapshot-based persistence (#28)
    Co-authored-by: orionji <orionji@icloud.com>

--- internal/engine/memdb/memdb_test.go
+++ internal/engine/memdb/memdb_test.go
@@ -3,6 +3,8 @@ package memdb_test
 import (
 	"context"
 	"errors"
+	"io/fs"
+	"os"
 	"strings"
 	"testing"
 	"time"
@@ -31,9 +33,26 @@ func parse(t *testing.T, cmd string, v any) {
 	}
 }
 
+func exists(t *testing.T, path string) bool {
+	t.Helper()
+	if _, err := os.Stat(path); err != nil {
+		if errors.Is(err, fs.ErrNotExist) {
+			return false
+		}
+		panic(err)
+	}
+	return true
+}
+
 func TestConfig(t *testing.T) {
 	var c memdb.Config
-	parse(t, "--memdb-retention-period 24h", &c)
+	parse(t, "--memdb-snapshot-path test.db --memdb-snapshot-interval 20s --memdb-retention-period 24h", &c)
+	if c.SnapshotPath != "test.db" {
+		t.Fail()
+	}
+	if c.SnapshotInterval != 20*time.Second {
+		t.Fail()
+	}
 	if c.RetentionPeriod != 24*time.Hour {
 		t.Fail()
 	}
@@ -48,6 +67,101 @@ func TestSuite(t *testing.T) {
 		t.Fatal(err)
 	}
 	engine.Test(t, g)
+}
+
+func TestSnapshot(t *testing.T) {
+	skipShort(t)
+	ctx := context.Background()
+	p := "test.db"
+	os.Remove(p)
+	g, err := memdb.New(&memdb.Config{
+		SnapshotPath:     p,
+		SnapshotInterval: 5 * time.Minute,
+		RetentionPeriod:  10 * time.Minute,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := g.Open(ctx); err != nil {
+		t.Fatal(err)
+	}
+
+	n := time.Now()
+	if _, err := g.InsertTasks(ctx, []*ratus.Task{
+		{
+			ID:        "1",
+			Topic:     "test",
+			State:     ratus.TaskStatePending,
+			Scheduled: &n,
+			Consumed:  &n,
+		},
+		{
+			ID:        "2",
+			Topic:     "test",
+			State:     ratus.TaskStatePending,
+			Scheduled: &n,
+			Consumed:  &n,
+		},
+	}); err != nil {
+		t.Fatal(err)
+	}
+
+	t.Run("chore", func(t *testing.T) {
+		if err := g.Chore(ctx); err != nil {
+			t.Error(err)
+		}
+		if !exists(t, p) {
+			t.Fail()
+		}
+		if err := g.Chore(ctx); err != nil {
+			t.Error(err)
+		}
+	})
+
+	t.Run("close", func(t *testing.T) {
+		if _, err := g.InsertTask(ctx, &ratus.Task{
+			ID:        "3",
+			Topic:     "test",
+			State:     ratus.TaskStatePending,
+			Scheduled: &n,
+			Consumed:  &n,
+		}); err != nil {
+			t.Error(err)
+		}
+		if err := g.Close(ctx); err != nil {
+			t.Error(err)
+		}
+		if !exists(t, p) {
+			t.Fail()
+		}
+	})
+
+	t.Run("recover", func(t *testing.T) {
+		u, err := memdb.New(&memdb.Config{
+			SnapshotPath:     p,
+			SnapshotInterval: 5 * time.Minute,
+			RetentionPeriod:  10 * time.Minute,
+		})
+		if err != nil {
+			t.Fatal(err)
+		}
+		if err := u.Open(ctx); err != nil {
+			t.Fatal(err)
+		}
+		v, err := u.ListTasks(ctx, "test", 10, 0)
+		if err != nil {
+			t.Error(err)
+		}
+		if len(v) != 3 {
+			t.Errorf("incorrect number of results, expected %d, got %d", 3, len(v))
+		}
+		if err := u.Destroy(ctx); err != nil {
+			t.Error(err)
+		}
+		if exists(t, p) {
+			t.Fail()
+		}
+	})
 }
 
 func TestExpire(t *testing.T) {
