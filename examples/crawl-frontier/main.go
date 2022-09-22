e0383481ce44d567c20bb72a20bd4b3a9eacf4bb
Yichao 'Peak' Ji <peakji@me.com>
2022-09-23 01:47:08 +0800

    test(example): add example for decoding payloads

--- examples/crawl-frontier/main.go
+++ examples/crawl-frontier/main.go
@@ -11,6 +11,15 @@ import (
 	"github.com/hyperonym/ratus"
 )
 
+// Payload contains information of a crawl task.
+type Payload struct {
+	URL   string
+	Trace struct {
+		Referrer string
+		Depth    int
+	}
+}
+
 func main() {
 
 	// Parse command-line flags to get the origin and create a client instance.
@@ -28,13 +37,13 @@ func main() {
 			ID:       "example.com",
 			Topic:    "fresh",
 			Producer: "mycrawler",
-			Payload:  "https://example.com",
+			Payload:  &Payload{URL: "https://example.com"},
 		},
 		{
 			ID:       "foobar.com",
 			Topic:    "fresh",
 			Producer: "mycrawler",
-			Payload:  "https://foobar.com",
+			Payload:  &Payload{URL: "https://foobar.com"},
 			Defer:    "5s",
 		},
 	}); err != nil {
@@ -63,21 +72,31 @@ func main() {
 				return
 			}
 
+			// Decode the payload of the task.
+			var p Payload
+			if err := ctx.Task.Decode(&p); err != nil {
+				log.Println(err)
+				return
+			}
+
 			// Print the current progress and sleep 2 seconds to "crawl" the URL.
-			log.Printf("[crawl]    %q\n", ctx.Task.Payload)
+			log.Printf("[crawl]    %q (referrer=%q)\n", p.URL, p.Trace.Referrer)
 			time.Sleep(2 * time.Second)
 
 			// Create tasks for the "newly discovered" URLs.
 			var ts []*ratus.Task
 			for i := 0; i < 3; i++ {
+				d := Payload{URL: fmt.Sprintf("%s/%d", p.URL, i)}
+				d.Trace.Referrer = p.URL
+				d.Trace.Depth = p.Trace.Depth + 1
 				t := ratus.Task{
 					ID:       fmt.Sprintf("%s/%d", ctx.Task.ID, i),
 					Topic:    "fresh",
 					Producer: "mycrawler",
-					Payload:  fmt.Sprintf("%s/%d", ctx.Task.Payload, i),
+					Payload:  &d,
 				}
 				ts = append(ts, &t)
-				log.Printf("[discover] %q\n", t.Payload)
+				log.Printf("[discover] %q (depth=%d)\n", d.URL, d.Trace.Depth)
 			}
 			if _, err := client.InsertTasks(ctx.Context, ts); err != nil {
 				log.Println(err)
@@ -105,8 +124,15 @@ func main() {
 				return
 			}
 
+			// Decode the payload of the task.
+			var p Payload
+			if err := ctx.Task.Decode(&p); err != nil {
+				log.Println(err)
+				return
+			}
+
 			// Print the current progress and sleep 2 seconds to "crawl" the URL.
-			log.Printf("[revisit]  %q\n", ctx.Task.Payload)
+			log.Printf("[revisit]  %q\n", p.URL)
 			time.Sleep(2 * time.Second)
 
 			// Revisit the URL after one minute.
