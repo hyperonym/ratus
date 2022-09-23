f5c549fd310c2d1fb3515d0a320a960d15b4d428
Yichao 'Peak' Ji <peakji@me.com>
2022-09-23 20:39:24 +0800

    test(model): remove duplicated test cases

--- ratus_test.go
+++ ratus_test.go
@@ -11,12 +11,71 @@ import (
 	"testing"
 	"time"
 
-	"go.mongodb.org/mongo-driver/bson"
-
 	"github.com/hyperonym/ratus"
 )
 
-func TestPayload(t *testing.T) {
+func TestEncode(t *testing.T) {
+	n := time.Now()
+	m := map[string]any{
+		"empty":  nil,
+		"bool":   true,
+		"int":    123,
+		"float":  3.14,
+		"string": "hello",
+		"array":  []any{1, 2, "a"},
+		"nested": map[string]any{
+			"empty":  nil,
+			"bool":   true,
+			"int":    123,
+			"float":  3.14,
+			"string": "hello",
+			"array":  []any{1, 2, "a"},
+		},
+	}
+	for _, x := range []struct {
+		name string
+		task *ratus.Task
+	}{
+		{"empty", &ratus.Task{ID: "1", Topic: "test", State: ratus.TaskStateActive, Scheduled: &n}},
+		{"bool", &ratus.Task{ID: "2", Topic: "test", State: ratus.TaskStateActive, Scheduled: &n, Payload: true}},
+		{"int", &ratus.Task{ID: "3", Topic: "test", State: ratus.TaskStateActive, Scheduled: &n, Payload: 123}},
+		{"float", &ratus.Task{ID: "4", Topic: "test", State: ratus.TaskStateActive, Scheduled: &n, Payload: 3.14}},
+		{"string", &ratus.Task{ID: "5", Topic: "test", State: ratus.TaskStateActive, Scheduled: &n, Payload: "hello"}},
+		{"array", &ratus.Task{ID: "6", Topic: "test", State: ratus.TaskStateActive, Scheduled: &n, Payload: []any{1, 2, "a"}}},
+		{"nested", &ratus.Task{ID: "7", Topic: "test", State: ratus.TaskStateActive, Scheduled: &n, Payload: m}},
+	} {
+		p := x
+		t.Run(p.name, func(t *testing.T) {
+			t.Parallel()
+			var buf bytes.Buffer
+			enc := gob.NewEncoder(&buf)
+			dec := gob.NewDecoder(&buf)
+			if err := enc.Encode(p.task); err != nil {
+				t.Error(err)
+			}
+			var v ratus.Task
+			if err := dec.Decode(&v); err != nil {
+				t.Error(err)
+			}
+			if v.ID != p.task.ID {
+				t.Fail()
+			}
+			if v.State != p.task.State {
+				t.Fail()
+			}
+			if !v.Scheduled.Round(time.Millisecond * 10).Equal(p.task.Scheduled.Round(time.Millisecond * 10)) {
+				t.Errorf("incorrect scheduled time, expected %v, got %v", p.task.Scheduled, v.Scheduled)
+			}
+			e, _ := json.Marshal(p.task.Payload)
+			a, _ := json.Marshal(v.Payload)
+			if !bytes.Equal(e, a) {
+				t.Errorf("incorrect payload, expected %s, got %s", string(e), string(a))
+			}
+		})
+	}
+}
+
+func TestDecode(t *testing.T) {
 	t.Run("empty", func(t *testing.T) {
 		t.Parallel()
 		var p any
@@ -110,124 +169,6 @@ func TestPayload(t *testing.T) {
 		}
 		if p.Name != "peak" || p.Date.Month != 7 || p.Date.Day != 29 {
 			t.Fail()
-		}
-	})
-}
-
-func TestEncoding(t *testing.T) {
-	n := time.Now()
-	m := map[string]any{
-		"empty":  nil,
-		"bool":   true,
-		"int":    123,
-		"float":  3.14,
-		"string": "hello",
-		"array":  []any{1, 2, "a"},
-		"nested": map[string]any{
-			"empty":  nil,
-			"bool":   true,
-			"int":    123,
-			"float":  3.14,
-			"string": "hello",
-			"array":  []any{1, 2, "a"},
-		},
-	}
-	ps := []struct {
-		name string
-		task *ratus.Task
-	}{
-		{"empty", &ratus.Task{ID: "1", State: ratus.TaskStateActive, Scheduled: &n}},
-		{"bool", &ratus.Task{ID: "1", State: ratus.TaskStateActive, Scheduled: &n, Payload: true}},
-		{"int", &ratus.Task{ID: "1", State: ratus.TaskStateActive, Scheduled: &n, Payload: 123}},
-		{"float", &ratus.Task{ID: "1", State: ratus.TaskStateActive, Scheduled: &n, Payload: 3.14}},
-		{"string", &ratus.Task{ID: "1", State: ratus.TaskStateActive, Scheduled: &n, Payload: "hello"}},
-		{"array", &ratus.Task{ID: "1", State: ratus.TaskStateActive, Scheduled: &n, Payload: []any{1, 2, "a"}}},
-		{"nested", &ratus.Task{ID: "1", State: ratus.TaskStateActive, Scheduled: &n, Payload: m}},
-	}
-
-	assert := func(expected, actual *ratus.Task) {
-		t.Helper()
-		if actual.ID != expected.ID {
-			t.Fail()
-		}
-		if actual.State != expected.State {
-			t.Fail()
-		}
-		if !actual.Scheduled.Round(time.Millisecond * 10).Equal(expected.Scheduled.Round(time.Millisecond * 10)) {
-			t.Errorf("incorrect scheduled time, expected %v, got %v", expected.Scheduled, actual.Scheduled)
-		}
-		e, _ := json.Marshal(expected.Payload)
-		a, _ := json.Marshal(actual.Payload)
-		if !bytes.Equal(e, a) {
-			t.Errorf("incorrect payload, expected %s, got %s", string(e), string(a))
-		}
-	}
-
-	t.Run("bson", func(t *testing.T) {
-		t.Parallel()
-		for _, x := range ps {
-			p := x
-			t.Run(p.name, func(t *testing.T) {
-				t.Parallel()
-				var b []byte
-
-				t.Run("encode", func(t *testing.T) {
-					var err error
-					b, err = bson.Marshal(p.task)
-					if err != nil {
-						t.Error(err)
-					}
-				})
-
-				t.Run("decode", func(t *testing.T) {
-					var v ratus.Task
-					if err := bson.Unmarshal(b, &v); err != nil {
-						t.Error(err)
-					}
-					assert(p.task, &v)
-				})
-			})
-		}
-
-		t.Run("error", func(t *testing.T) {
-			t.Parallel()
-			b, err := bson.Marshal(&struct {
-				Topic int `bson:"topic"`
-			}{1})
-			if err != nil {
-				t.Error(err)
-			}
-			var f ratus.Task
-			if err := bson.Unmarshal(b, &f); err == nil {
-				t.Fail()
-			}
-		})
-	})
-
-	t.Run("binary", func(t *testing.T) {
-		t.Parallel()
-		for _, x := range ps {
-			p := x
-			t.Run(p.name, func(t *testing.T) {
-				t.Parallel()
-				var buf bytes.Buffer
-				enc := gob.NewEncoder(&buf)
-				dec := gob.NewDecoder(&buf)
-
-				t.Run("encode", func(t *testing.T) {
-					if err := enc.Encode(p.task); err != nil {
-						t.Error(err)
-					}
-				})
-
-				t.Run("decode", func(t *testing.T) {
-					var v ratus.Task
-					if err := dec.Decode(&v); err != nil {
-						t.Error(err)
-					}
-					assert(p.task, &v)
-				})
-			})
 		}
 	})
 }
